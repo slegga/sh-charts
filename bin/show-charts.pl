9ecb07dbca494616dd4e27aacf7c33348bda428d
Stein Hammer <steihamm@gmail.com>
2021-03-28 17:11:07 +0200

    Rename

--- bin/show-charts.pl
+++ bin/show-charts.pl
@@ -14,9 +14,9 @@ show-charts.pl
 
 =head1 SYNOPSIS
 
-    CHART_CONFIG_FILE=manualtest/chess.conf morbo bin/show-graphs.pl
-    CHART_CONFIG_FILE=manualtest/temperature.conf morbo bin/show-graphs.pl
-    CHART_CONFIG_FILE=manualtest/weight.conf morbo bin/show-graphs.pl
+    CHART_CONFIG_FILE=manualtest/chess.conf morbo bin/show-charts.pl
+    CHART_CONFIG_FILE=manualtest/temperature.conf morbo bin/show-charts.pl
+    CHART_CONFIG_FILE=manualtest/weight.conf morbo bin/show-charts.pl
 
 =head1 DESCRIPTION
 
